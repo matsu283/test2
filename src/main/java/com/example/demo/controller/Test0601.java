9a31dd6ec9b3d56e1139d40ded5549f9fa3047d9
nakashima <nakashima-takaha@outlook.jp>
2023-06-01 15:05:04 +0900

    5555名

--- src/main/java/com/example/demo/controller/Test0601.java
+++ src/main/java/com/example/demo/controller/Test0601.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test0601 {
 
-	@PostMapping("/post")
+	@GetMapping("/post")
 	public String post(@RequestParam("name")String name,
 			@RequestParam("age") int age) {
 		return "名前は" + name +"、年齢は" + age + "です。";
