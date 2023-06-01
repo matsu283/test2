b087137a7cac3f91657af149ff5b422f04d4818c
Itsuki <jorirei-64315@outlook.jp>
2023-06-01 16:17:47 +0900

    Test0601_02の修正

--- src/main/java/com/example/demo/controller/Test0601_02.java
+++ src/main/java/com/example/demo/controller/Test0601_02.java
@@ -2,7 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class Test0601_02 {
 
 	@GetMapping("/post1")
