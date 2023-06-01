b690d4a4aedf090c70c8075e8da7153f044265de
nakashima <nakashima-takaha@outlook.jp>
2023-06-01 15:01:30 +0900

    12回目

--- src/main/java/com/example/demo/controller/Test0601.java
+++ src/main/java/com/example/demo/controller/Test0601.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test0601 {
 
+	@PostMapping("/post")
+	public String post(@RequestParam("name")String name,
+			@RequestParam("age") int age) {
+		return "名前は" + name +"、年齢は" + age + "です。";
+	}
 }
