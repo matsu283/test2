f7e425f1bd87ccfd78f74bfed8bbef95e89085b9
matsu <aeon283@outlook.jp>
2023-06-01 11:19:04 +0900

    最初のコミットです。

---
+++ src/main/java/com/example/demo/Ordersysdb2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ordersysdb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ordersysdb2Application.class, args);
+	}
+
+}
