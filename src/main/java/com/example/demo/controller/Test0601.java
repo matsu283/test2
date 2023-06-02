7943dcbc870e235d410ed4cd0cbe13328a745d00
Itsuki <jorirei-64315@outlook.jp>
2023-06-02 09:17:24 +0900

    practice_113.htmlへの更新 @櫻井

--- src/main/java/com/example/demo/controller/Test0601.java
+++ src/main/java/com/example/demo/controller/Test0601.java
@@ -9,6 +9,6 @@ public class Test0601 {
 	@GetMapping("/post")
 	public String post(@RequestParam("name")String name,
 			@RequestParam("age") int age) {
-		return "名前は" + name +"、年齢は" + age + "です。";
+		return "名前は" + name + "、年齢は" + age + "です。";
 	}
 }
